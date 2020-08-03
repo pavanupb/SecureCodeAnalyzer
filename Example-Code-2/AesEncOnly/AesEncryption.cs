5547ca268e8c4569226362ab2183481d7ad0ac76
pavangb <38906373+pavanupb@users.noreply.github.com>
2020-08-03 12:20:15 +0200

    Added Example Code

---
+++ Example-Code-2/AesEncOnly/AesEncryption.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AesEncOnly
+{
+    public class AesEncryption
+    {
+        public byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+        {
+            string textToEncrypt = "Test String";
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] cipherText;
+            
+            using (SymmetricAlgorithm aesAlg = SymmetricAlgorithm.Create("AES"))
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                var originalText = Encoding.UTF8.GetBytes(textToEncrypt);
+                cipherText = encryptor.TransformFinalBlock(originalText, 0, originalText.Length);
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return cipherText;
+        }
+    }
+}
---
+++ Example-Code-2/AesEncOnly/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace AesEncOnly
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string original = "This is secret message";            
+            byte[] key = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
+            byte[] iv = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
+
+            // Create a new instance of the Aes
+            // class.  This generates a new key and initialization
+            // vector (IV).
+            using (SymmetricAlgorithm myAes = SymmetricAlgorithm.Create("AES"))
+            {
+                myAes.Key = key;
+                myAes.IV = iv;
+
+                AesEncryption encryptDecrypt = new AesEncryption();
+
+                byte[] ciphetText = encryptDecrypt.EncryptStringToBytes_Aes(original, myAes.Key, myAes.IV);
+
+                //Display the original data and the decrypted data.
+                Console.WriteLine("Original:   {0}", original);
+            }
+        }
+    }
+}
