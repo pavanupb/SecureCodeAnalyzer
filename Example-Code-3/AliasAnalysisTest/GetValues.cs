0fe7ae7440cc69d6a1294c787bd6aff021f11da3
pavangb <38906373+pavanupb@users.noreply.github.com>
2020-08-03 15:23:38 +0200

    Added Alias Code Example

---
+++ Example-Code-3/AliasAnalysisTest/GetValues.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AliasAnalysisTest
+{
+    public class GetValues
+    {
+        public string foo(string recievedText)
+        {
+            string recievedAliasedText = recievedText;
+            return recievedAliasedText;
+        }
+    }
+}
---
+++ Example-Code-3/AliasAnalysisTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AliasAnalysisTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secret = "This is a secret message";
+            string secretAlias = secret;
+
+            GetValues getValues = new GetValues();
+            string taintedText = getValues.foo(secretAlias);
+
+            Console.WriteLine($"Tainted Text is {taintedText}");
+        }
+    }
+}
